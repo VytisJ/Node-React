91e4523698e87f97bb82087371fc2f8d3734cc5d
VytisJ <vytis1133@gmail.com>
2023-09-08 17:56:54 +0300

    10 paskaita prideta

---
+++ event_management/src/components/Dropdown.jsx
@@ -0,0 +1,29 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+// import "./Dropdown.scss";
+
+// const Dropdown = ({ value, onChange, options, placeholder }) => {
+//   return (
+//     <div className="select-city">
+//       <select value={value} onChange={onChange}>
+//         <option value="" disabled hidden>
+//           {placeholder}
+//         </option>
+//         {options.map((city, index) => (
+//           <option key={index} value={city}>
+//             {city}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// };
+
+// Dropdown.propTypes = {
+//   value: PropTypes.string.isRequired,
+//   onChange: PropTypes.func.isRequired,
+//   options: PropTypes.arrayOf(PropTypes.string).isRequired,
+//   placeholder: PropTypes.string.isRequired,
+// };
+
+// export default Dropdown;
