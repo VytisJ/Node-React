52aad907294d015651251aa824b78aab06fbc89c
VytisJ <vytis1133@gmail.com>
2023-09-21 09:39:56 +0300

    update

--- marketplace_vj/routes/const.js
+++ marketplace_vj/routes/const.js
@@ -6,7 +6,11 @@ export const HOME_PATH = "/";
 export const LOGIN_PATH = "/login";
 export const REGISTER_PATH = "/register";
 
-export const topNavigationItems = [{ title: "Home", path: HOME_PATH }];
+export const topNavigationItems = [
+  { title: "Home", path: HOME_PATH },
+  { title: "Login", path: LOGIN_PATH, hideOnAuth: true },
+  { title: "Register", path: REGISTER_PATH, hideOnAuth: true },
+];
 
 export const routes = [
   { path: HOME_PATH, Component: Home },
